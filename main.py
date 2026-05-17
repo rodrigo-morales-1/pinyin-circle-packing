f68416c136d65a3d386432e503cb8ba56d2a50b8
Rodrigo Morales <rodrigo@morales.pe>
2026-05-17 10:58:45 -0500

    many changes

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import circlify
 import matplotlib.pyplot as plt
 import os
 import json
+import time
+import datetime
+
+start_time = time.time()
 
 plt.rcParams["font.family"] = "Noto Serif CJK SC"
 
@@ -17,7 +21,8 @@ circles = circlify.circlify(
 )
 
 # Create just a figure and only one subplot
-fig, ax = plt.subplots(figsize=(14, 14))
+figsize = 50
+fig, ax = plt.subplots(figsize=(figsize, figsize))
 
 # Title
 ax.set_title('拼音分组汉字图')
@@ -36,15 +41,18 @@ lim = max(
 plt.xlim(-lim, lim)
 plt.ylim(-lim, lim)
 
+circles_line_width=0.5
+pinyin_boxes_line_width=0.5
+
 for circle in circles:
     # Circles in depth level 1
     if circle.level == 1:
         x, y, r = circle
-        ax.add_patch(plt.Circle((x, y), r, facecolor="white", edgecolor="black"))
+        ax.add_patch(plt.Circle((x, y), r, facecolor="white", edgecolor="black", linewidth=circles_line_width))
     # Circles in depth level 2: Pinyin without accents
     if circle.level == 2:
         x, y, r = circle
-        ax.add_patch(plt.Circle((x, y), r, facecolor="white", edgecolor="black"))
+        ax.add_patch(plt.Circle((x, y), r, facecolor="white", edgecolor="black", linewidth=circles_line_width))
         # Add label
         label = circle.ex["id"]
         plt.annotate(label, (x, y + r), va='center', ha='center',
@@ -52,11 +60,12 @@ for circle in circles:
                          facecolor='white',
                          edgecolor='black',
                          boxstyle='round',
+                         linewidth=pinyin_boxes_line_width,
                          pad=.25))
     # Circles in depth level 3: Pinyin with accents
     if circle.level == 3:
         x, y, r = circle
-        ax.add_patch(plt.Circle((x, y), r, facecolor="white", edgecolor="black"))
+        ax.add_patch(plt.Circle((x, y), r, facecolor="white", edgecolor="black", linewidth=circles_line_width))
         # Add label
         label = circle.ex["id"]
         plt.annotate(label, (x, y + r), va='center', ha='center',
@@ -64,6 +73,7 @@ for circle in circles:
                          facecolor='white',
                          edgecolor='black',
                          boxstyle='round',
+                         linewidth=pinyin_boxes_line_width,
                          pad=.25))
     # Circles in depth level 4: Chinese characters
     elif circle.level == 4:
@@ -73,5 +83,16 @@ for circle in circles:
         label = circle.ex["id"]
         plt.annotate(label, (x, y), va='center', ha='center', color="black")
 
-plt.savefig(os.path.basename(__file__) + '.png', dpi=300)
+
+# Save PNG image
+os.chdir(os.path.dirname(__file__))
+dpi = 300
+output_file = f'python-pinyin-dpi-{dpi}-figsize-{figsize}-' + time.strftime("%Y-%m-%dT%H-%M-%S") + '.png'
+plt.savefig(output_file, dpi=dpi)
+
+# Print execution time
+elapsed_seconds = time.time() - start_time
+elapsed_time = datetime.timedelta(seconds=elapsed_seconds)
+print(f"--- Execution time: {elapsed_time} ---")
+
 
